b19d044e72d0ae6985a697347d8de0a4c1ff62c6
doni63 <dbatistajunior63@gmail.com>
2021-12-11 19:35:23 -0300

    Exercicios Vetores Dio

---
+++ src/array/ExercicioVetor2.java
@@ -0,0 +1,36 @@
+package array;
+
+import java.util.Scanner;
+
+public class ExercicioVetor2 {
+
+	public static void main(String[] args) {
+		// Faça um programa que leia um vetor de 6 caracteres, e diga quantas consoantes foram lidas. Imprima consoantes
+		Scanner sc = new Scanner(System.in);
+		String[] consoantes = new String[6];
+		int quantidadeConsoantes = 0;
+		
+		int count = 0;
+		do {
+			String letra = sc.next();
+			if(!(letra.equalsIgnoreCase("a") ||
+					letra.equalsIgnoreCase("e") ||
+					letra.equalsIgnoreCase("i") ||
+					letra.equalsIgnoreCase("o") ||
+					letra.equalsIgnoreCase("u"))) {
+				consoantes[count] = letra;
+				quantidadeConsoantes ++;
+			}
+			
+			count++;
+		}while(count < consoantes.length);
+		
+		System.out.print("\nConsoantes digitados: ");
+		for (String string : consoantes) {
+			if(string != null)
+			System.out.print(string + " ");
+		}
+		System.out.print("\nQuantidade de consoantes digitadas: " + quantidadeConsoantes);
+	}
+
+}
