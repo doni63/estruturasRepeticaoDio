337a61cafda56eb4c5fbc0b736ea90af1b6819e0
doni63 <dbatistajunior63@gmail.com>
2021-12-13 15:46:31 -0300

    Atualização

--- src/interruptores/ExercicioWhile.java
+++ src/interruptores/ExercicioWhile.java
@@ -1,25 +1,21 @@
 package interruptores;
+
 // Crie um programa com laço for de 1 a 14, sendo que somente mútiplos de 
 //2 sjam impressos. Entretant, quando o loop passar pelo 12, o mesmo deve ser abortado.
 public class ExercicioWhile {
 
 	public static void main(String[] args) {
 		int[] vetor = new int[14];
-		for(int i = 0; i < vetor.length; i++) {
-			vetor[i] = (i+1);
+		for (int i = 0; i < vetor.length; i++) {
+			vetor[i] = (i + 1);
 		}
-		
+
 		for (int i : vetor) {
-			if(i % 2 == 0) {
-				if(i != 12)
-					System.out.print(i + " ");
-				else
-					break;
+			if (i % 2 == 0 && i != 12) {
+				System.out.print(i + " ");
 			}
-			
+			else if(i == 12) 
+				break;
 		}
-		
-
 	}
-
 }
